e066a3dca12f3e127ec2142453d4cf64cc70918d
vishchi <vishalsc1999@gmail.com>
2020-06-26 20:51:24 +0530

    Initial Commit

---
+++ cms/admin/include/footer.php
@@ -0,0 +1,4 @@
+<p>&nbsp;</p>
+<p><small>&copy; MCC CMS&nbsp;</small></p>
+</body>
+</html>
---
+++ cms/users/includes/footer.php
@@ -0,0 +1,6 @@
+ <footer class="site-footer">
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
